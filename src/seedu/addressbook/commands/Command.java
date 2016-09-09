999ab81b47fec236ad25f3bbc1463e87a55ea914
Samson <samsontmr@users.noreply.github.com>
2016-09-09 17:46:08 +0800

    Add isMutating method to Command class and its child classes

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -42,6 +42,12 @@ public abstract class Command {
     public abstract CommandResult execute();
 
     /**
+     * 
+     * @return true for command types that mutate the data.
+     */
+    public abstract boolean isMutating();
+    
+    /**
      * Supplies the data the command will operate on.
      */
     public void setData(AddressBook addressBook, List<? extends ReadOnlyPerson> relevantPersons) {
